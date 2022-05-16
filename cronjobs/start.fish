6e4d70d544e2fd11bfc55ffe4d9a29d814efeaa8
Mathias Millet <megabigfoutoir@gmail.com>
2022-05-16 23:44:42 +0200

    remove cronjobs leftovers

--- cronjobs/start.fish
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env fish
-
-echo "STARTING SCRAPPER SERVICE"
-
-set workdir (dirname (status --current-filename))
-cd $workdir
-source .venv/bin/activate.fish
-
-python -m scrapper loop --journald-logs Cronjobs[$INSTANCE_NAME]
