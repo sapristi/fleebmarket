ebab13567247a419fd2a109efb109164ae87561e
Mathias Millet <megabigfoutoir@gmail.com>
2022-05-17 00:03:51 +0200

    move fleebmarket_django to backend folder

--- backend/fleebmarket/runtime_setup.py
+++ backend/fleebmarket/runtime_setup.py
@@ -4,13 +4,9 @@ from pathlib import Path  # Python 3.6+ only
 from dotenv import load_dotenv
 
 root_path = Path(__file__).absolute().parent.parent.parent
-env_paths = [
-    root_path.parent / ".env",
-    root_path / ".env",
-]
-
+print("ROOT", root_path)
+env_paths = root_path / ".env"
 print("Loading .env from", env_paths)
-for path in env_paths:
-    load_dotenv(dotenv_path=path)
+load_dotenv(dotenv_path=env_paths)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fleebmarket.settings")
