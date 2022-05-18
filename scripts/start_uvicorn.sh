cc7528f0b23f2fcdea39e6f15a591f1b42428f69
Mathias Millet <mmmm@mailden.eu>
2022-05-18 17:40:14 +0000

    various fixes

--- scripts/start_uvicorn.sh
+++ scripts/start_uvicorn.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-set -a
-source $INSTANCE_PATH/backend/.env
-set +a
 
-source $VENV_PATH/bin/activate
-cd $INSTANCE_PATH/backend/fleebmarket_django
+source $INSTANCE_PATH/backend/.venv/bin/activate
+cd $INSTANCE_PATH/backend
 uvicorn fleebmarket.asgi:app --forwarded-allow-ips "*" --port ${UVICORN_PORT:-8000}
