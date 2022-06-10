f0046f7a8b832adde39bb6335bf8901d9f385e45
Mathias Millet <megabigfoutoir@gmail.com>
2022-06-10 20:47:34 +0200

    feat(cronjobs): add alert and monitor tasks

---
+++ backend/fleebmarket/tasks.py
@@ -0,0 +1,22 @@
+import logging
+
+from django.conf import settings
+from fleebmarket.utils import alerts, monitor
+from huey import crontab
+from huey.contrib.djhuey import periodic_task
+
+logger = logging.getLogger(__name__)
+
+
+@periodic_task(crontab(minute="00"))
+def send_alerts():
+    alerts.send(
+        discord_token=settings.DISCORD_CREDS["bot_token"],
+        since_hours=1,
+        test_channel=False,
+    )
+
+
+@periodic_task(crontab(minute="00"))
+def send_monitor_metrics():
+    monitor.put()
